310874d50c392c904ff13e6deafbc6c81a08e061
trome23 <romennoodles23@yahoo.com>
2019-05-14 09:24:44 -0600

    changed connection.js for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    port     : 3306,
-    user     : "root",
-    password : 'password',
-    database : 'burgers_db'
-  });
-   
- // Make connection.
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'burgers_db'
+    })
+};
+
+connection.connect();
   
   // Export connection for our ORM to use.
   module.exports = connection;
