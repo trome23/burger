759d6bdcf2c10f5c59d54e47078103716265ee6b
trome23 <romennoodles23@yahoo.com>
2019-05-15 18:25:01 -0600

    another update to server.js

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ var routes = require("../burger/controllers/burgers_controller")
 
 app.use('/', routes);
 
+app.use(timeout(15000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
+
 app.listen(PORT, function() {
     console.log('our server is up and running on port: ' + PORT);;
 })
